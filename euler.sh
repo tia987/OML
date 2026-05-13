18b55cd1855638594cbb8cff87a64da4fbb19b02
tia987 <mlucca@ethz.ch>
2026-05-13 09:10:18 +0200

    Adapted Euler script

--- euler.sh
+++ euler.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=AML25            # Job name    (default: sbatch)
-#SBATCH --output=aml.out            # Output file (default: slurm-%j.out)
-#SBATCH --error=aml.err             # Error file  (default: slurm-%j.out)
+#SBATCH --job-name=0ML26            # Job name    (default: sbatch)
+#SBATCH --output=oml.out            # Output file (default: slurm-%j.out)
+#SBATCH --error=oml.err             # Error file  (default: slurm-%j.out)
 #SBATCH --nodes=1                   # Number of nodes
 #SBATCH --ntasks=1                  # Number of tasks
 #SBATCH --ntasks-per-node=1         # Number of tasks per node
 #SBATCH --constraint=EPYC_7763      # Select node with CPU
-#SBATCH --mem-per-cpu=32024          # Memory per CPU
+#SBATCH --mem-per-cpu=1024          # Memory per CPU
 #SBATCH --time=24:00:00             # Wall clock time limit
 #SBATCH --mail-type=END,FAIL        # Send an email when job ends
 
@@ -17,8 +17,14 @@ module list
 # Load environment
 source installation.sh
 
+# Install required packages (source ./installation.sh)
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
+
 # Go into folder
-cd ex_1/
+cd OptML_CNN_project
 
 # Run the program
-python3 main.py -val=0 --save_params=1 --load_params=0
+python3 main.py -n_trials=100
