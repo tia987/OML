fcd624470cbf6e0eeb7188d77e43c4e27a74b248
tia987 <mlucca@ethz.ch>
2026-05-15 19:04:06 +0200

    Added new task, and changed parser to traskX

--- OptML_CNN_project/main.py
+++ OptML_CNN_project/main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 from tasks.task_0_baseline_cnn import *
 from tasks.task_1_hyperparameter import *
+from tasks.task_2_robustness import *
 
 from optuna.visualization.matplotlib import (
     plot_optimization_history,
@@ -17,14 +18,14 @@ from optuna.visualization.matplotlib import (
 if __name__ == "__main__":
     # Get parser arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-mode", "--mode", choices=["search", "train"], default="search", help="Search best parameters or just train (sarch/train)")
+    parser.add_argument("-mode", "--mode", choices=["task0", "task1", "task2"], default="search", help="Select task (taskX)")
     parser.add_argument("-n_trials", "--n_trials", type=int, default=10, help="Number of trials to add.")
     args = parser.parse_args()
 
     study_name = "fracture_classification"
     best_search_optuna = f"sqlite:///tasks/task_1_hyperparameter/results/best_search_optuna.db"
 
-    if args.mode == "search":
+    if args.mode == "task1":
         study = optuna.create_study(study_name=study_name, storage=best_search_optuna, load_if_exists=True, direction="maximize")
     
         study.optimize(objective, n_trials=args.n_trials)
@@ -42,7 +43,7 @@ if __name__ == "__main__":
          # Save Optimization History
         fig1 = plot_optimization_history(study)
         fig1.figure.savefig(
-            os.path.join(".", "optimization_history.png"),
+            os.path.join(".", "tasks/task_1_hyperparameter/optimization_history.png"),
             dpi=300,
             bbox_inches="tight",
         )
@@ -51,15 +52,22 @@ if __name__ == "__main__":
         # Save Parameter Importances
         fig2 = plot_param_importances(study)
         fig2.figure.savefig(
-            os.path.join(".", "param_importances.png"),
+            os.path.join(".", "tasks/task_1_hyperparameter/param_importances.png"),
             dpi=300,
             bbox_inches="tight",
         )
         plt.close(fig2.figure)
 
-    elif args.mode == "train":
+    elif args.mode == "task0":
         try:
             study = optuna.load_study(study_name=study_name, storage=best_search_optuna)
             main(study.best_params)
+        except KeyError:
+            print("No existing study found. Please run with --mode search first.")
+
+    elif args.mode == "task2":
+        try:
+            study = optuna.load_study(study_name=study_name, storage=best_search_optuna)
+            noise(study.best_params)
         except KeyError:
             print("No existing study found. Please run with --mode search first.")
